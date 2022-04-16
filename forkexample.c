709267e3c508e5ee610f52533a6b417443a0f7c2
milo2112 <mjrp.ros@gmail.com>
2022-04-15 21:42:28 -0400

    adding control to handle values < 0

--- forkexample.c
+++ forkexample.c
@@ -4,10 +4,18 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-    printf(" \nbefore fork flag\n");
+   
     int id = fork();
-    printf(" \nDisplaying from fork id: %d\n\n\n\n", id);
-    // printf(" \nDisplaying flag\n");
-    return 0;
+    if (id > 0)
     
-}
+        printf(" \nDisplaying id from Parent fork process: %d\n\n\n\n", id);
+    else
+    
+        printf(" \nDisplaying id from Child fork process: %d\n\n\n\n", id);
+   
+    if (id < 0)
+        printf(" \nError creating fork process", id);
+
+    
+    return 0;
+}    
